b96ce9aa35a2dc2f8749c11ed8efe589f06c70aa
Rahulsingarapu <rahulsingarapu092@gmail.com>
2023-12-19 13:11:07 +0530

    commit changes

--- ui/src/app/pages/sample/sample-upload/scanned-card.tsx
+++ ui/src/app/pages/sample/sample-upload/scanned-card.tsx
@@ -1,4 +1,4 @@
-import { Button, Card } from "antd";
+import { Button, Card, Col, Row } from "antd";
 import Meta from 'antd/es/card/Meta';
 import image from '../../../../assets/Picture1.png';
 import { useLocation, useParams } from "react-router-dom";
@@ -32,28 +32,33 @@ export default function ScannedCard(){
   }
 
     return(
-        <>
-        <Card cover={<img alt="example" src={'http://172.20.50.169/design_room/dist/services/kanban-service/upload-files/'+ data[0]?.fileName} style={{ width: '50%', height: '60%' ,background:''}} />}>
+        <div >
+        <Row gutter={24} >
+           <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 6 }} lg={{ span: 6 }} xl={{ span: 9}} >
+           <Card hoverable style={{background:'#3f4648'}} cover={<img alt="example" src={'http://172.20.50.169/design_room/dist/services/kanban-service/upload-files/'+ data[0]?.fileName} />}>
         <Meta
-          title="Sample Digital Card"
+          title={<div style={{ color: 'white' }}><b>Sample Digital Card</b></div>}
           description={
-            <div className="print">
-              <div>Brand Name :{data[0]?.brandName} </div>
-              <div>Style No : {data[0]?.styleNo}</div>
-              <div>Item No : {data[0]?.itemNo}</div>
-              <div>Item Description : {data[0]?.itemDescription}</div>
-              <div>Category : {data[0]?.categoryName}</div>
-              <div>Season : {data[0]?.seasonName}</div>
-              <div>Fabric Content : {data[0]?.fabricContent}</div>
-              <div>Fabric Count : {data[0]?.fabricCount}</div>
-              <div>GSM : {data[0]?.gsm}</div>
-              <div>FOB : {data[0]?.fob}</div>
-              <div>Qty/Season : {data[0]?.qtyPerSeason}</div>
-              <div>Location : {data[0]?.locationName}</div>
+            <div style={{color:'white'}} className="print">
+              <div >Brand Name &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {data[0]?.brandName} </div>
+              <div>Style No &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {data[0]?.styleNo}</div>
+              <div>Item No &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {data[0]?.itemNo}</div>
+              <div>Item Description &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {data[0]?.itemDescription}</div>
+              <div>Category &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {data[0]?.categoryName}</div>
+              <div>Season &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {data[0]?.seasonName}</div>
+              <div>Fabric Content &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {data[0]?.fabricContent}</div>
+              <div>Fabric Count &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {data[0]?.fabricCount}</div>
+              <div>GSM &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {data[0]?.gsm}</div>
+              <div>FOB &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {data[0]?.fob}</div>
+              <div>Qty/Season &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {data[0]?.qtyPerSeason}</div>
+              <div>Location &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {data[0]?.locationName}</div>
             </div>
           }
         />
       </Card>
-        </>
+           </Col>
+        </Row>
+
+        </div>
     )
 }
