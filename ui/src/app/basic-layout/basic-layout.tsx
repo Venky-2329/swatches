a18f89406c341203d67ecc019770c5e7f35cf1cb
chefKush <kushalms001@gmail.com>
2024-03-16 14:44:17 +0530

    swatch Card basic layout

--- ui/src/app/basic-layout/basic-layout.tsx
+++ ui/src/app/basic-layout/basic-layout.tsx
@@ -1,97 +1,137 @@
 import styles from './basic-layout.module.css';
-import { Link, Outlet, HashRouter as Router, useNavigate } from 'react-router-dom';
+import {
+  Link,
+  Outlet,
+  HashRouter as Router,
+  useNavigate,
+} from 'react-router-dom';
 import { ProBreadcrumb, ProConfigProvider } from '@ant-design/pro-components';
+import { RiScissorsCutLine } from 'react-icons/ri';
 import ProLayout from '@ant-design/pro-layout';
 import { LightModeIcon } from '../icons/lightmode.icon';
 import { treeRouter } from '../utils/common';
 import { DarkModeIcon } from '../icons/darkmode.icon';
 import { useState } from 'react';
 import { Button, Tooltip, theme } from 'antd';
-import { UserOutlined, RobotOutlined, LogoutOutlined, TableOutlined, PicCenterOutlined,UploadOutlined,HomeOutlined } from '@ant-design/icons'
+import {
+  UserOutlined,
+  RobotOutlined,
+  LogoutOutlined,
+  TableOutlined,
+  PicCenterOutlined,
+  UploadOutlined,
+  HomeOutlined,
+} from '@ant-design/icons';
 import { LogoIcon } from '../icons/logo-icon';
 
-const { useToken } = theme
+const { useToken } = theme;
 /* eslint-disable-next-line */
-export interface BasicLayoutProps { }
+export interface BasicLayoutProps {}
 
 export function BasicLayout(props: BasicLayoutProps) {
   const [pathname, setPathname] = useState(location.pathname);
   const [dark, setDark] = useState(false);
   const navigate = useNavigate();
-  const { token: { colorPrimary, colorPrimaryActive, colorPrimaryBg } } = useToken()
+  const {
+    token: { colorPrimary, colorPrimaryActive, colorPrimaryBg },
+  } = useToken();
   const createUser: any = JSON.parse(localStorage.getItem('auth'));
-  const user = createUser.userName
+  const user = createUser.userName;
 
   function handleLogout() {
-    localStorage.clear()
-    navigate('/login')
+    localStorage.clear();
+    navigate('/login');
   }
 
   const baseRouterList: any[] = [
     {
-      label: "Home",
-      key: "home-screen",
-      path: "home-screen",
-      icon:<HomeOutlined />,
-      filepath: "../",
+      label: 'Home',
+      key: 'home-screen',
+      path: 'home-screen',
+      icon: <HomeOutlined />,
+      filepath: '../',
     },
     {
-        label:"Masters",
-        path: "/",
-        icon: <PicCenterOutlined />,
-        filepath: "../",
-        children :[
-          {
-            label: "User",
-            key: "user-grid",
-            path: "user-grid",
-            icon: <TableOutlined />,
-            filepath: "../",
-          },
-          {
-            label: "Brands",
-            key: "brands-grid",
-            path: "brands-grid",
-            icon: <TableOutlined />,
-            filepath: "../",
-          },
-          {
-            label: "Category",
-            key: "category-grid",
-            path: "category-grid",
-            icon: <TableOutlined />,
-            filepath: "../",
-          },
-          {
-            label: "Season",
-            key: "season-grid",
-            path: "season-grid",
-            icon: <TableOutlined />,
-            filepath: "../",
-          },
-          {
-            label: "Location",
-            key: "location-grid",
-            path: "location-grid",
-            icon: <TableOutlined />,
-            filepath: "../",
-          },
-        ]
+      label: 'Masters',
+      path: '/masters',
+      icon: <PicCenterOutlined />,
+      filepath: '../',
+      children: [
+        {
+          label: 'User',
+          key: 'user-grid',
+          path: 'user-grid',
+          icon: <TableOutlined />,
+          filepath: '../',
+        },
+        {
+          label: 'Brands',
+          key: 'brands-grid',
+          path: 'brands-grid',
+          icon: <TableOutlined />,
+          filepath: '../',
+        },
+        {
+          label: 'Category',
+          key: 'category-grid',
+          path: 'category-grid',
+          icon: <TableOutlined />,
+          filepath: '../',
+        },
+        {
+          label: 'Season',
+          key: 'season-grid',
+          path: 'season-grid',
+          icon: <TableOutlined />,
+          filepath: '../',
+        },
+        {
+          label: 'Location',
+          key: 'location-grid',
+          path: 'location-grid',
+          icon: <TableOutlined />,
+          filepath: '../',
+        },
+      ],
     },
     {
-      label: "Sample Upload",
-      key: "sample-view",
-      path: "sample-view",
+      label: 'Swatch Card ',
+      path: '/swatch-card',
+      icon: <RiScissorsCutLine />,
+      filepath: '../',
+      children: [
+        {
+          label: 'Fabric Upload',
+          key: 'fabric-view',
+          path: 'fabric-view',
+          icon: <UploadOutlined />,
+          filepath: '../',
+        },
+        {
+          label: 'Trims Upload',
+          key: 'trims-view',
+          path: 'trims-view',
+          icon: <UploadOutlined />,
+          filepath: '../',
+        },
+      ],
+    },
+    {
+      label: 'Sample Upload',
+      key: 'sample-view',
+      path: 'sample-view',
       icon: <UploadOutlined />,
-      filepath: "../",
+      filepath: '../',
     },
+
     {
-      label: "Design Studio",
-      key: "sample-cards",
-      path: "sample-cards",
+      label: 'Design Studio',
+      key: 'sample-cards',
+      path: 'sample-cards',
       icon: <RobotOutlined />,
-      filepath: "../",
+      filepath: '../',
     },
+
     // {
     //   label: "Samples View",
     //   key: "sample-view",
@@ -99,10 +139,9 @@ export function BasicLayout(props: BasicLayoutProps) {
     //   icon: <TableOutlined />,
     //   filepath: "../",
     // },
-    
-  ]
+  ];
   return (
-    <ProConfigProvider dark={dark}  >
+    <ProConfigProvider dark={dark}>
       <div
         id="main-layout"
         style={{
@@ -111,13 +150,20 @@ export function BasicLayout(props: BasicLayoutProps) {
       >
         <ProLayout
           title="Digital Design Room"
-          logo={<LogoIcon  />}
-          locale='en-US'
+          logo={<LogoIcon />}
+          locale="en-US"
           // layout='top'
-          colorPrimary='#035199'
-          headerContentRender={(props) => props.layout !== 'side' && document.body.clientWidth > 1000 ? <ProBreadcrumb /> : undefined}
+          colorPrimary="#035199"
+          headerContentRender={(props) =>
+            props.layout !== 'side' && document.body.clientWidth > 1000 ? (
+              <ProBreadcrumb />
+            ) : undefined
+          }
           fixSiderbar
-          token={{ header: { colorBgHeader: 'transparent' }, sider: { colorBgMenuItemSelected: colorPrimaryBg } }}
+          token={{
+            header: { colorBgHeader: 'transparent' },
+            sider: { colorBgMenuItemSelected: colorPrimaryBg },
+          }}
           route={{
             path: '/',
             routes: treeRouter(baseRouterList),
@@ -129,7 +175,7 @@ export function BasicLayout(props: BasicLayoutProps) {
             src: <UserOutlined />,
             size: 'small',
             title: user,
-            style: { color: colorPrimary }
+            style: { color: colorPrimary },
           }}
           contentStyle={{ paddingBlock: '10px', paddingInline: '10px' }}
           actionsRender={(props) => {
@@ -148,12 +194,17 @@ export function BasicLayout(props: BasicLayoutProps) {
                   onClick={() => {
                     setDark(!dark);
                   }}
-                  icon={!dark ? <DarkModeIcon style={{ color: "#035199" }} /> : <LightModeIcon style={{ color: "#035199" }} />}
+                  icon={
+                    !dark ? (
+                      <DarkModeIcon style={{ color: '#035199' }} />
+                    ) : (
+                      <LightModeIcon style={{ color: '#035199' }} />
+                    )
+                  }
                 ></Button>
               </Tooltip>,
-              <Tooltip placement="bottom" title={"Sign Out"}>
+              <Tooltip placement="bottom" title={'Sign Out'}>
                 <Button
-
                   size="large"
                   // style={{ borderRadius: "5px" }}
                   icon={
@@ -161,7 +212,7 @@ export function BasicLayout(props: BasicLayoutProps) {
                       style={{ color: '#035199' }}
                       onClick={async () => {
                         // await signOut(dispatch);
-                        handleLogout()
+                        handleLogout();
                       }}
                     />
                   }
@@ -172,16 +223,16 @@ export function BasicLayout(props: BasicLayoutProps) {
           menuItemRender={(item, dom) => {
             return (
               <Link
-                to={item?.path || "/"}
+                to={item?.path || '/'}
                 onClick={() => {
-                  setPathname(item.path || "/");
+                  setPathname(item.path || '/');
                 }}
               >
                 {dom}
               </Link>
             );
           }}
-          onMenuHeaderClick={() => navigate("/")}
+          onMenuHeaderClick={() => navigate('/')}
         >
           <div style={{ padding: '10px' }}>
             <Outlet />
